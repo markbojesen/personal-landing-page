e324984b5faf442348eb64f6a29ea2c82a04e742
Mark Bojesen <bojesenmark@gmail.com>
2017-07-15 10:34:15 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+    // $('#main-static-text').typeIt({
+    //  speed: 100,
+    //  autoStart: true,
+    //  cursor: true
+    // })
+    // .tiSettings({speed: 100})
+    // .tiType('Front End Developer ')
+    // .tiPause(100)
+    // .tiType('/ ')
+    // .tiType('Designer');
+
+    $(document).ready(function() {
+      $('#main-static-text').hide().delay(1000).fadeIn(2200);
+});
+
+    $(document).ready(function() {
+      $('.social').hide().delay(3000).fadeIn(2200);
+});
